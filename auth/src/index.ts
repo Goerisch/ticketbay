76e3e28441c28f7a6694e45e8447279dfdbcbd37
Dominic Immanuel Görisch <dominic.goerisch@nordlb.de>
2023-05-26 16:41:07 +0200

    JWT-Token added

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import {json} from 'body-parser';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import {currentUserRouter} from './routes/current-user';
 import {signupRouter} from './routes/signup';
@@ -11,7 +12,16 @@ import {errorHandler} from './middlewares/error-handler';
 import {NotFoundError} from './errors/not-found-error';
 
 const app = express();
+//allows connection to ingress nginx via proxy
+app.set('trust proxy', true);
 app.use(json());
+//enforces HTTPS, but the Cookie won't be encrypted itself
+app.use(
+    cookieSession({
+        signed: false,
+        secure: true,
+    }),
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -25,6 +35,10 @@ app.all('/*', async (req, res) => {
 app.use(errorHandler);
 
 const start = async () => {
+    if (!process.env.JWT_KEY) {
+        throw new Error('JWT_KEY must be defined!');
+    }
+
     try {
         await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
         console.log('Successfully connected to MongoDB');
