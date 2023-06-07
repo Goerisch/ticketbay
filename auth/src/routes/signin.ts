101f381928497a05bca8a4b9aa959d54476c7095
Dominic Immanuel Görisch <dominic.goerisch@nordlb.de>
2023-06-07 20:48:15 +0200

    fixed imports

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,11 +1,10 @@
 import express, {Request, Response} from 'express';
 import {body} from 'express-validator';
 import jwt from 'jsonwebtoken';
+import {validateRequest, BadRequestError} from '@dgticketbay/common';
 
 import {User} from '../models/user';
-import {validateRequest} from '../middlewares/validate-request';
 import {Password} from '../services/password';
-import {BadRequestError} from '../errors/bad-request-error';
 
 const router = express.Router();
 
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,9 +2,9 @@ import express, {Request, Response} from 'express';
 import {body} from 'express-validator';
 import jwt from 'jsonwebtoken';
 
-import {validateRequest} from '../middlewares/validate-request';
+import {validateRequest} from '@dgticketbay/common';
 import {User} from '../models/user';
-import {BadRequestError} from '../errors/bad-request-error';
+import {BadRequestError} from '@dgticketbay/common';
 
 const router = express.Router();
 
