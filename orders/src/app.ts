60d2252adcd97ddfa6c8bed494110bcdc16fea77
Dominic Immanuel Görisch <dominic.goerisch@nordlb.de>
2023-08-01 17:06:36 +0200

    orderService initial setup

---
+++ orders/src/app.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import 'express-async-errors';
+import {json} from 'body-parser';
+import cookieSession from 'cookie-session';
+import {errorHandler, NotFoundError, currentUser} from '@dgticketbay/common';
+import {createOrderRouter} from './routes/new';
+import {showOrderRouter} from './routes/show';
+import {indexOrderRouter} from './routes';
+import {deleteOrderRouter} from './routes/delete';
+
+const app = express();
+//allows connection to ingress nginx via proxy
+app.set('trust proxy', true);
+app.use(json());
+//enforces HTTPS, but the Cookie won't be encrypted itself
+app.use(
+    cookieSession({
+        signed: false,
+        secure: process.env.NODE_ENV !== 'test',
+    }),
+);
+app.use(currentUser);
+app.use(createOrderRouter);
+app.use(showOrderRouter);
+app.use(indexOrderRouter);
+app.use(deleteOrderRouter);
+
+app.all('/*', async (req, res) => {
+    throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export {app};
